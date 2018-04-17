87a6b76370802df04aa7c516f8e157a9e826ac0d
GuZhongCai <835166582@qq.com>
2018-04-17 16:28:56 +0800

    做完了  不做了

--- app/src/main/java/com/dalimao/mytaxi/module/nearby/NearbyPresenter.java
+++ app/src/main/java/com/dalimao/mytaxi/module/nearby/NearbyPresenter.java
@@ -22,41 +22,7 @@ public class NearbyPresenter implements NearbyContract.INearbyPresenter {
     // 当前的订单
     private Order mCurrentOrder;
 
-    /**
-     * 接收子线程消息的 Handler
-     */
-    static class MyHandler extends Handler {
 
-//        // 弱引用
-//        WeakReference<MainPresenterImpl> dialogRef;
-//        public MyHandler(MainPresenterImpl presenter)
-//        {
-//            dialogRef = new WeakReference<MainPresenterImpl>(presenter);
-//        }
-//        @Override
-//        public void handleMessage(Message msg) {
-//            MainPresenterImpl presenter = dialogRef.get();
-//            if (presenter == null) {
-//                return;
-//            }
-//            // 处理UI 变化
-//            switch (msg.what) {
-//                case IAccountManager.LOGIN_SUC:
-//                    // 登录成功
-//                    presenter.view.showLoginSuc();
-//                    break;
-//                case IAccountManager.TOKEN_INVALID:
-//                    // 登录过期
-//                    presenter.view.showError(IAccountManager.TOKEN_INVALID, "");
-//                    break;
-//                case IAccountManager.SERVER_FAIL:
-//                    // 服务器错误
-//                    presenter.view.showError(IAccountManager.SERVER_FAIL, "");
-//                    break;
-//            }
-//
-//        }
-    }
 
     @RegisterBus
     public void onLoginResponse(LoginResponse loginResponse) {
